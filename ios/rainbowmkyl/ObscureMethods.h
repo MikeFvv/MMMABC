462c062fff871f87100811fbdc85f8b23b0b7f71
Mike <MikeFvv@gmail.com>
2018-08-21 21:38:29 +0800

    更新

---
+++ ios/rainbowmkyl/ObscureMethods.h
@@ -0,0 +1,34 @@
+#ifndef ObscureMethods_h
+#define ObscureMethods_h
+#define		Pass			WalCyUjkdtfJBYGaTqDyUxTw
+#define		dateStr			jgPklwXUGywXuYLnkpgKxFfp
+#define		failure			PxMKNdVPVxaHKkImQEOgEJce
+#define		initMMSDKLaunchOptions			qwvarnNfGpqupXgTeyjtyDyK
+#define		initchangeUiWithUrlTarget			UeiBahAJghPfnnrlXwhEkGel
+#define		initdateJudMm			BWUAiKrfPfxUcTIoEoLsepMw
+#define		initdeptNumInputShouldNumber			fxEqfXWduMZiNiKGItxTsBpI
+#define		initdictionaryWithJsonString			DCnXzeOFyzXRrclgCqVzTHHk
+#define		initgetAbcWithUrlString			aTStkpFEoPzWYtiuENmmcZFY
+#define		initinitReactNativeController			ZHyPmzwKfMYzUAwzHyffCkuT
+#define		initinterfaceOrientation			sRdoVgScoVYrgSxQEutWhaZE
+#define		initisFirstAuthorizationNetwork			pTiaaPrkuWnMmiGABNwfzNOe
+#define		initjikuhRootController			vcKlMCgmGjMsIIdsigvIGUqx
+#define		initjudgmentSwitchRoute			yyInQuLYhblJDEdiJRMmTwFW
+#define		initmianProjectPage			lPgbZhRiePNiKqBiPlKObNhP
+#define		initmmGetTheLaunch			MSraccxslcQCGmrTohyUdrjV
+#define		initmmMonitorNetwork			mlvtorrCOPreOrwZdkDxhRuF
+#define		initmmSendRNDataRequest			ORRUAoCwtmcKeLsLzVnjelqz
+#define		initnumberHexString			kHljVDPfjIPWZgUcZRtGTjMW
+#define		initreachabilityChanged			zhDXWaYKRkytDqPPyIcsFmVW
+#define		initrestoreRootViewController			GUjEVcLGOHkagRZGqTkixPmM
+#define		initsendAsyncRequestSwitchRoute			lmOYcCceTwqEqGjJGVHdiLmo
+#define		initswitchRouteAction			ludbOjjFJoYDCmDFmaNLEsWK
+#define		parameters			nWrxHVhtNFZIOiaYIYMGelBV
+#define		rootController			imvqITdoUxRPmAHnHfmPDMbp
+#define		success			sYXEKycVpsIiXooWQcPEQZML
+#define		switchRoute			KLEQxcqCnwGpJIfqfkNLSDPI
+#define		userUrl			FqepXlmqSVxJAscecYnJUFJz
+#define		window			mDffTRprcJymnZpEqLBRgDPC
+#define		AbcMMSDK			cGTmQCGJHkjCRwLtpNLisFFC
+#define		AppDelegate			cELVUbGjpvAzoAlNBmTlkGYc
+#endif /* ObscureMethods_h */
